60a568521258e0216b0abbc85e9a7a1a179d8622
Fedor Borshev <fedor@borshev.com>
2022-09-17 12:10:19 +0300

    Building anonymized data dump

---
+++ roles/pg-master/templates/anonymous_dump/dump.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -e
+
+SRC="postgres://{{ app_postgresql_user }}:{{ app_postgresql_password }}@{{ postgresql_host }}:{{ postgresql_port }}/{{
+app_postgresql_database }}?sslmode=require"
+DST=/home/circle/anon_production_dump.sql.gz
+
+dump=$(mktemp /tmp/anon-pg-dump.XXXXXX)
+
+echo Dumping data...
+docker run --network host --rm -v /root/anonymous_dump/config:/app -w /app datanymizer/pg_datanymizer:0.6.0 -c config.yml --accept_invalid_certs $SRC -- --no-owner --no-privileges>$dump
+
+filesize=$(stat --format="%s" $dump)
+
+if [ $filesize -le 100000 ]; then
+  echo "Dump failed, filesize < 100k"
+  rm -f $dump
+  exit 127
+fi;
+
+echo Raw dump size is $(numfmt --to=si $filesize)
+echo Compressing...
+
+gzip -c $dump > $DST
+
+filesize=$(stat --format="%s" $DST)
+if [ $filesize -le 10000 ]; then
+  echo "Dump compression failed, filesize < 10k"
+  rm -f $dump
+  exit 127
+fi;
+
+chown circle /home/circle/anon_production_dump.sql.gz
+
+echo Compressed dump saved to to $DST
+echo Final size is $(numfmt --to=si $filesize)
+
+echo Pinging healthchecks.io...
+curl https://hc-ping.com/d8870f50-f07a-4feb-986a-53e4362f5367
